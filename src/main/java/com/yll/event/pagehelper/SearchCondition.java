185062fce35f782ee9cf21bca0b4901873091381
夜林蓝 <1562098029@qq.com>
2024-05-25 17:20:46 +0800

    1.解决PageSize pageNum  接口访问为空的问题

--- src/main/java/com/yll/event/pagehelper/SearchCondition.java
+++ src/main/java/com/yll/event/pagehelper/SearchCondition.java
@@ -1,6 +1,7 @@
 package com.yll.event.pagehelper;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
@@ -8,10 +9,12 @@ import javax.validation.constraints.NotNull;
  * 条件搜索父类
  * @author 夜林蓝
  */
+@Data
 public class SearchCondition {
 	@ApiModelProperty("页数")
 	@NotNull
 	private Integer pageNum;
 	@ApiModelProperty("单页条数")
+	@NotNull
 	private Integer pageSize;
 }
